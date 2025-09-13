f084a52a8f417938f07bfcd3477f30d5e8962dfd
Queue <p.5313@hotmail.com>
2025-09-14 01:08:34 +0700

    develop | feat(structure): layouts

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
