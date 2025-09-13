b854cf22bc2c159072ca2a01db0499a3d564d8eb
Queue <p.5313@hotmail.com>
2025-09-14 00:59:38 +0700

    develop | feat(css): tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -7,5 +8,11 @@ export default defineNuxtConfig({
     config: {
       standalone: false,
     },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
